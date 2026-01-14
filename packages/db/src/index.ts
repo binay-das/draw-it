0706b9d43a16e400a6fba3852ce6668e34acbee7
Binay <binaydas1123@gmail.com>
2026-01-14 20:58:57 +0530

    bug fix

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,10 +1,25 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
+
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
 declare global {
     var prisma: PrismaClient | undefined;
 }
 
-const prisma = global.prisma ?? new PrismaClient();
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+const prisma = global.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') {
     global.prisma = prisma;
