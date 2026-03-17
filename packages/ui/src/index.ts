684be2ed8666ad9e5e8e1077f99ca063523a1145
Binay <binaydas1123@gmail.com>
2026-03-17 09:31:53 +0530

    feat(ui): add shadcn dialog, badge, tooltip, separator and label components to ui package

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -3,4 +3,9 @@
 
 export * from "./components/ui/button";
 export * from "./components/ui/input";
+export * from "./components/ui/dialog";
+export * from "./components/ui/badge";
+export * from "./components/ui/tooltip";
+export * from "./components/ui/separator";
+export * from "./components/ui/label";
 export * from "./lib/utils";
